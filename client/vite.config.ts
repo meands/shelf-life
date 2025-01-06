37740e2f93c5fd18a09be2d18c812d9312cf3693
Mandy <mandyyj1207@gmail.com>
2025-01-06 21:20:37 +0000

    fix tabler icon loading time

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2549771202
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
+});
